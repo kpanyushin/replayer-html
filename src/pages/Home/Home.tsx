6ef6c5943b0d159ba89adbefb79bb924bd3eea35
Kirill Panyushin <kapanyu1@mts.ru>
2020-11-05 16:40:28 +0300

    Add home layout"

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { getHandsHistory } from '_redux/replayer/slice';
-// import { dataSelector } from '_redux/replayer/selectors';
+import { dataSelector } from '_redux/replayer/selectors';
+
+import s from './Home.module.scss';
 
 const Home = () => {
   const [file, setFile] = useState(null);
   const dispatch = useDispatch();
-  const data = useSelector(state => state);
+  const data = useSelector(dataSelector);
 
   const handleFileUpload = (e: any) => {
     if (e.target.files && e.target.files.length) {
@@ -15,16 +17,31 @@ const Home = () => {
     }
   };
 
-  const handleSendClick = () => {
+  const handleSendClick = (e: any) => {
+    e.preventDefault();
     dispatch(getHandsHistory(file));
   };
 
   console.log(data);
 
   return (
-    <div className="App">
-      <input type="file" onChange={handleFileUpload} />
-      <button onClick={handleSendClick}>Отправить</button>
+    <div className={s.root}>
+      <div className={s.uploadWrapper}>
+        <div className={s.header}>
+          Hand Converter
+        </div>
+        <form className={s.content} onSubmit={handleSendClick}>
+          <textarea
+            className={s.textarea}
+            rows={7}
+            placeholder="Paste your hand history or upload file. Use a blank line to separate mupliple hands"
+          />
+          <input className={s.file} type="file" onChange={handleFileUpload} />
+          <button className={s.submitBtn} type="submit" disabled={!file}>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
